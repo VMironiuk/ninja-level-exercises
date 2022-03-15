03f79c24ac56e2d277e65014ea17ce6fae751757
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-03-15 22:09:18 +0200

    Add exercise-010 of ninja-level-4

---
+++ ninja-level-4/exercise-010/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james":      {"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": {"James Bond", "Literature", "Computer Science"},
+		"no_dr":           {"Being evil", "Ice cream", "Sunsets"},
+	}
+
+	m["john_smith"] = []string{"Popular", "Chocolate"}
+
+	delete(m, "no_dr")
+
+	for k, s := range m {
+		fmt.Println("Record", k)
+		for i, v := range s {
+			fmt.Printf("\tindex: %v\tvalue: %v\n", i, v)
+		}
+	}
+}
